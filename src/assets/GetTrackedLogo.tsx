61e515cf166d17ccc13b9967f4bc51fc561726b4
Misha <mishaoliynyk1301@gmail.com>
2025-04-16 17:24:40 +0300

    add user layout components

--- src/assets/GetTrackedLogo.tsx
+++ src/assets/GetTrackedLogo.tsx
@@ -1,22 +1,12 @@
-// import React from 'react';
-// import { Tabs } from '@chakra-ui/react';
-/** @jsxRuntime classic */
-/** @jsx jsx */
 import SvgWrapper from '@/components/wrappers/SvgWrapper.tsx';
-import { jsx} from '@emotion/react';
 import getTrackedLogoSvg from './getTrackedLogo.svg';
 
-function GetTrackedLogo(props: {height: string}) {
-  
+function GetTrackedLogo(props: { height: string }) {
+
   return (
     <SvgWrapper height={props.height}>
-      <img
-        css={{
-          objectFit: 'contain',
-          height: '100%',
-        }}
-        src={getTrackedLogoSvg}
-      />
+      <img className="h-full object-contain"
+        src={getTrackedLogoSvg} />
     </SvgWrapper>
   );
 }
