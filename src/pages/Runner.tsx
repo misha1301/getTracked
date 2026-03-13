ecb6fa3e6e6c103634d1d3a69244208a81d717df
Misha <mishaoliynyk1301@gmail.com>
2026-03-14 00:52:18 +0200

    Integrate scrollarea to table and finalize table design

--- src/pages/Runner.tsx
+++ src/pages/Runner.tsx
@@ -6,6 +6,7 @@ import {
   useReactTable,
   ColumnDef,
   createColumnHelper,
+  getPaginationRowModel,
 } from '@tanstack/react-table';
 import { Outlet } from 'react-router-dom';
 
@@ -68,6 +69,9 @@ import {
 } from "@/components/ui/popover"
 import { Tracker } from '@/db/schema';
 import { tableTrackersDataSet } from '@/db/dataSample';
+import { ScrollAreaFlex, ScrollBar } from '@/components/ui/scroll-area';
+import { Corner } from '@radix-ui/react-scroll-area';
+import PaginationModel from '@/components/pagination/PaginationModel';
 
 const frameworks = [
   {
@@ -116,47 +120,52 @@ export function DataTable<TData>({
 }: DataTableProps<TData>) {
 
   return (
-    <section className={cn('flex w-full flex-col overflow-hidden [--data-table-head-height:40px] [--data-table-padding:5px] [--data-table-header-px:13px] [--data-table-body-rounded:13px] [--data-table-bg:#1e1e1e] [--data-table-cell-bg:#181818]', className)} {...props}>
-      <div className="relative before:absolute before:left-0 before:top-0 before:z-1 before:h-(--data-table-head-height) before:w-(--data-table-header-px) before:bg-linear-to-r before:from-(--data-table-bg) before:to-transparent after:absolute after:right-0 after:top-0 after:z-1 after:h-(--data-table-head-height) after:w-(--data-table-header-px) after:bg-linear-to-l after:from-(--data-table-bg) after:to-transparent">
-        <div className="after:absolute after:inset-0 after:top-(--data-table-head-height) after:bg-(--data-table-cell-bg) after:left-[calc(var(--data-table-padding)*1)] after:right-[calc(var(--data-table-padding)*1)] after:z-[-1] after:rounded-(--data-table-body-rounded)">
-          <div className='overflow-hidden rounded-(--data-table-body-rounded) before:pointer-events-none before:absolute before:inset-0 before:top-(--data-table-head-height) before:left-[calc(var(--data-table-padding)*1)]  before:right-[calc(var(--data-table-padding)*1)] before:z-1 before:rounded-(--data-table-body-rounded) before:ring-[calc(var(--data-table-padding)+5px)] before:ring-(--data-table-bg) after:pointer-events-none after:absolute after:inset-0 after:top-(--data-table-head-height) after:left-[calc(var(--data-table-padding)*1)] after:right-[calc(var(--data-table-padding)*1)] after:z-2 after:rounded-(--data-table-body-rounded) after:bg-clip-padding after:shadow-[0px_0px_5px_1px_rgba(47,_47,_47,_0.25)]'>
-
-            <Table className='z-2'>
-              <TableHeader className='[&_tr]:border-0 bg-(--data-table-bg)'>
-                {table.getHeaderGroups().map((headerGroup) => (
-                  <TableRow key={headerGroup.id}>
-                    {headerGroup.headers.map((header) => (
-                      <TableHead key={header.id} colSpan={header.colSpan} className='font-[montserrat] font-normal text-(--gtr-color-muted-foreground)'>
-                        {header.isPlaceholder
-                          ? null
-                          : flexRender(header.column.columnDef.header, header.getContext())}
-                      </TableHead>
-                    ))}
-                  </TableRow>
+    <section className={cn('flex w-full flex-col pb-0 transition-[padding] duration-300 [&:has([data-slot="scroll-area-scrollbar"])]:pb-[15px] overflow-hidden [--data-table-head-height:40px] [--data-table-padding:5px] [--data-table-header-px:13px] [--data-table-body-rounded:13px] [--data-table-bg:#1e1e1e] [--data-table-cell-bg:#181818]', className)} {...props}>
+      <ScrollAreaFlex
+        className={cn(
+          'relative  w-full scroll-smooth bg-(--data-table-bg) whitespace-nowrap transition-[border-radius] duration-300 rounded-b-[16px] [&:has([data-slot="scroll-area-scrollbar"])]:rounded-b-[0px]'
+        )}
+        type="auto"
+        viewportClassName='pb-[5px] px-[5px]'
+      >
+        <Table className='[&:has(thead_tr:hover)]:bg-muted/50 rounded-(--data-table-body-rounded)'>
+          <TableHeader className='[&_tr]:border-0 bg-(--data-table-bg)'>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id} className='[&_th:first-child]:rounded-tl-(--data-table-body-rounded) [&_th:last-child]:rounded-tr-(--data-table-body-rounded)'>
+                {headerGroup.headers.map((header) => (
+                  <TableHead key={header.id} colSpan={header.colSpan} className='font-[montserrat] font-normal text-(--gtr-color-muted-foreground)'>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(header.column.columnDef.header, header.getContext())}
+                  </TableHead>
                 ))}
-              </TableHeader>
-              <TableBody className='relative border-(--data-table-bg)  rounded-(--data-table-body-rounded)'>
-                {table.getRowModel().rows?.length ? (
-                  table.getRowModel().rows.map((rowModel) => (
-                    <TableRow key={rowModel.id} data-state={rowModel.getIsSelected() && 'selected'} className=''>
-                      {rowModel.getVisibleCells().map((cell) => (
-                        <TableCell key={cell.id} >
-                          {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                        </TableCell>
-                      ))}
-                    </TableRow>
-                  ))
-                ) : (
-                  <TableRow>
-                    <TableCell colSpan={table.getAllColumns().length} className='h-24 text-center'>
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody className=' relative after:content-[""] after:absolute after:inset-0 after:rounded-(--data-table-body-rounded) after:shadow-[0px_0px_5px_1px_rgba(47,47,47,0.25)]
+                                after:pointer-events-none [&_tr:last-child_td:first-child]:rounded-bl-(--data-table-body-rounded) [&_tr:last-child_td:last-child]:rounded-br-(--data-table-body-rounded) [&_tr:first-child_td:first-child]:rounded-tl-(--data-table-body-rounded) [&_tr:first-child_td:last-child]:rounded-tr-(--data-table-body-rounded)'>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((rowModel) => (
+                <TableRow key={rowModel.id} data-state={rowModel.getIsSelected() && 'selected'} className='bg-(--data-table-cell-bg)'>
+                  {rowModel.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id} className=''>
+                      {flexRender(cell.column.columnDef.cell, cell.getContext())}
                     </TableCell>
-                  </TableRow>
-                )}
-              </TableBody>
-            </Table>
-          </div>
-        </div>
-      </div>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell colSpan={table.getAllColumns().length} className='h-24 text-center'>
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+        <Corner></Corner>
+        <ScrollBar orientation='horizontal' style={{ bottom: "-9px", left:"8px", right:"8px", background: "#181818" }} className='h-[8px]'>
+        </ScrollBar>
+      </ScrollAreaFlex>
     </section>
   );
 }
@@ -232,6 +241,7 @@ export const RunnerList = () => {
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
   })
 
   const [open, setOpen] = React.useState(false)
@@ -247,7 +257,7 @@ export const RunnerList = () => {
         </SectionUtils>
       </ContentSection>
 
-      <ContentBoard>
+       <ContentBoard>
         <BoardHeader className='px-[2px_10px]'>
           <HeaderIcon>
             <StatusIndicator type={IndicatorStatus.Active} />
@@ -270,7 +280,7 @@ export const RunnerList = () => {
               </LogicBlock>
               <ErrorLabel>Недопустиме значення електронної адреси!</ErrorLabel>
             </LogicBox>
-            {/*<LogicBox>*/}
+            {/* <LogicBox>*/}
             {/*  <LogicBoxLabel>*/}
             {/*    <p>Rule</p>*/}
             {/*  </LogicBoxLabel>*/}
@@ -283,7 +293,7 @@ export const RunnerList = () => {
             {/*      placeholder='select: div \n children: a'*/}
             {/*    />*/}
             {/*  </LogicBlock>*/}
-            {/*</LogicBox>*/}
+            {/*</LogicBox> */}
             <LogicBox aria-invalid='false' className='group'>
               <LogicBoxLabel>
                 <span>Is paginable</span>
@@ -389,7 +399,7 @@ export const RunnerList = () => {
           </ContentGroup>
         </BoardContent>
         <BoardFooter>Last updated / 13.11.2024</BoardFooter>
-      </ContentBoard>
+      </ContentBoard> 
 
       <ContentSection>
         <SectionTitle>
@@ -428,15 +438,23 @@ export const RunnerList = () => {
         <SectionUtils></SectionUtils>
       </ContentSection>
 
-      <ContentBoard>
+     <ContentBoard className='p-0'>
         <BoardContent>
           <LogicBox>
             <LogicBoxLabel muted className='mt-[8px]'>Trackers view</LogicBoxLabel>
             <DataTable table={table} />
-
+            <PaginationModel></PaginationModel>
           </LogicBox>
+
         </BoardContent>
       </ContentBoard>
+
+      <ContentSection>
+        <SectionTitle>
+          <h3 className='text-[17px] font-semibold'>Trackers list</h3>{' '}
+        </SectionTitle>
+        <SectionUtils></SectionUtils>
+      </ContentSection>
     </ContentFrame>
   );
 };
